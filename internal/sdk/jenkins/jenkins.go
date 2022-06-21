5dcac2eea5bb4af4b4c137bffcef6ad1a136a4e1
csothen <cesarjcpinheiro@gmail.com>
2022-06-21 19:50:19 +0100

    Improve Jenkins

--- internal/sdk/jenkins/jenkins.go
+++ internal/sdk/jenkins/jenkins.go
@@ -1,7 +1,9 @@
 package jenkins
 
 import (
+	"bytes"
 	"fmt"
+	"io/ioutil"
 	"net/http"
 )
 
@@ -27,4 +29,40 @@ func Status(host string) (StatusCode, error) {
 		return Down, fmt.Errorf("could not check instance status")
 	}
 	return Up, nil
+}
+
+func Setup(host string) error {
+	jobName := "default"
+	createJobURL := fmt.Sprintf("http://%s:8080/job/createItem?name=%s", host, jobName)
+	triggerJobURL := fmt.Sprintf("http://%s:8080/job/%s/build", host, jobName)
+
+	data, err := ioutil.ReadFile("config.xml")
+	if err != nil {
+		return err
+	}
+
+	client := &http.Client{}
+	// create the request to create the job
+	cjReq, err := http.NewRequest(http.MethodPost, createJobURL, bytes.NewBuffer(data))
+	if err != nil {
+		return fmt.Errorf("could not build request: %w", err)
+	}
+
+	// Add content type header
+	cjReq.Header.Add("Content-Type", "text/xml")
+	_, err = client.Do(cjReq)
+	if err != nil {
+		return err
+	}
+
+	tjReq, err := http.NewRequest(http.MethodPost, triggerJobURL, nil)
+	if err != nil {
+		return fmt.Errorf("could not build request: %w", err)
+	}
+
+	_, err = client.Do(tjReq)
+	if err != nil {
+		return err
+	}
+	return nil
 }
--- templates/sonarqube/files/run-ansible.sh
+++ templates/sonarqube/files/run-ansible.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
 
-sudo apt update && sudo apt upgrade -y
 sudo apt-add-repository ppa:ansible/ansible -y
 sudo apt update
 sudo apt install ansible -y
